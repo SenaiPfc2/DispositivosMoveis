4f3eb78b5ad7bef5e512ea52d31d69e3bbb499f7
crist <crist@DESKTOP-0S6T0CB>
2017-11-14 02:30:57 -0200

    commit teste

---
+++ banco/bancoaulaphp.sql
@@ -0,0 +1,23 @@
+﻿# Host: localhost  (Version 5.5.5-10.1.26-MariaDB)
+# Date: 2017-11-14 02:29:37
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "tbusuarios"
+#
+
+DROP TABLE IF EXISTS `tbusuarios`;
+CREATE TABLE `tbusuarios` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `NomeUser` varchar(70) DEFAULT NULL,
+  `Email` varchar(30) DEFAULT NULL,
+  `Senha` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "tbusuarios"
+#
+
+INSERT INTO `tbusuarios` VALUES (6,'teste 11','teste1@gmail.com','1234'),(7,'Cris Stef','cris.stef28@gmail.com','123456');
